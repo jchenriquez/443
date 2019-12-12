c2ee45ced3015ed51330b64bc7252bfb30320b06
jchenriquez <jc.henriquez04@gmail.com>
2019-12-12 09:17:20 -0500

    Fixed the byte creating but still have to fix the left count.

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
-import ("strconv"
-        "fmt"
+import (
+	"fmt"
+	"strconv"
 )
 
-func addNum (arr[] byte, num int) int {
+func addNum(arr []byte, num int) int {
 	numStr := strconv.Itoa(num)
 
 	for i := 0; i < len(numStr); i++ {
@@ -20,40 +21,47 @@ func compress(chars []byte) int {
 		return 0
 	}
 
-  if len(chars) == 1 {
-    return 1
-  }
-
+	if len(chars) == 1 {
+		return 1
+	}
 	currCharIndex := 0
-  resultLength:=0
+	resultLength := 0
 
 	for i := 1; i < len(chars); i++ {
 
 		if chars[i] != chars[currCharIndex] || (i == len(chars)-1 && i-currCharIndex >= 1) {
-      var subArr []byte
-      var numSteps int
+			var subArr []byte
+			var numSteps int
 
-      if i < len(chars)-1 {
-        subArr = chars[currCharIndex+1:i]
-        numSteps = i - (currCharIndex) 
-      } else {
-        subArr = chars[currCharIndex+1:i+1]
-        numSteps = (i+1) - currCharIndex
-      }
-      numLength := addNum(subArr, numSteps)
-      currCharIndex+=(numLength+1)
-      chars[currCharIndex] = chars[i]
-      resultLength+=(numLength+1)
+			if i < len(chars)-1 {
+				subArr = chars[currCharIndex+1 : i]
+				numSteps = i - currCharIndex
+			} else {
+				subArr = chars[currCharIndex+1 : i+1]
+				numSteps = i - currCharIndex
+			}
+
+			if numSteps > 1 {
+				numLength := addNum(subArr, numSteps)
+				currCharIndex += numLength + 1
+				chars[currCharIndex] = chars[i]
+				resultLength += numLength + 1
+
+			} else {
+				currCharIndex += 1
+				resultLength++
+			}
+
 		}
 
 	}
 
-  return resultLength
+	return resultLength
 }
 
 func main() {
- arr := []byte{'a', 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c'}
- length := compress(arr)
+	arr := []byte{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c'}
+	length := compress(arr)
 
- fmt.Printf("compressed %s, length %d", arr, length)
+	fmt.Printf("compressed %s, length %d", arr, length)
 }
