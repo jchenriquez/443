c29983db623b41bfd14ad56df192a926dcc02ef5
jchenriquez <jc.henriquez04@gmail.com>
2019-12-12 13:08:12 -0500

    Adding fix that prevented the last character from getting copied.

--- main.go
+++ main.go
@@ -26,49 +26,50 @@ func compress(chars []byte) int {
 	}
 
 	currCharIndex := 0
-  nextCharPlacement := 0
+	nextCharPlacement := 0
 	resultLength := 0
 
 	for i := 1; i < len(chars); i++ {
-		if chars[i] != chars[currCharIndex] || i == len(chars) - 1 {
-      var distance int
-      lastIsDiff := false
+		if chars[i] != chars[currCharIndex] || i == len(chars)-1 {
+			var distance int
+			lastIsDiff := false
 
-      if i == len(chars) - 1 {
-        if chars[i] != chars[currCharIndex] {
-          distance = i - currCharIndex
-          resultLength++
-          lastIsDiff = true
-        } else {
-          distance = i+1 - currCharIndex
-        }
-      } else {
-        distance = i - currCharIndex
-      }
+			if i == len(chars)-1 {
+				fmt.Printf("chars[i] %c, chars[currCharIndex] %c\n", chars[i], chars[currCharIndex])
+				if chars[i] != chars[currCharIndex] {
+					distance = i - currCharIndex
+					resultLength++
+					lastIsDiff = true
+				} else {
+					distance = i + 1 - currCharIndex
+				}
+			} else {
+				distance = i - currCharIndex
+			}
 
-      distanceDigitLength := distance
+			distanceDigitLength := distance
 
-      chars[nextCharPlacement] = chars[currCharIndex]
-      if distance > 1 {
-        if i - (nextCharPlacement+1) > 0 {
-          distanceDigitLength = addNum(chars[nextCharPlacement+1: i], distance)
-        } else {
-          distanceDigitLength = addNum(chars[nextCharPlacement+1: i+1], distance)
-        }
-      }
+			chars[nextCharPlacement] = chars[currCharIndex]
+			if distance > 1 {
+				if i-(nextCharPlacement+1) > 0 {
+					distanceDigitLength = addNum(chars[nextCharPlacement+1:i], distance)
+				} else {
+					distanceDigitLength = addNum(chars[nextCharPlacement+1:i+1], distance)
+				}
+			}
 
-      nextCharPlacement += distanceDigitLength
-      currCharIndex = i 
-      resultLength += distanceDigitLength
+			nextCharPlacement += distanceDigitLength
+			currCharIndex = i
+			resultLength += distanceDigitLength
 
-      if distance > 1 {
-        nextCharPlacement++
-        resultLength++
+			if distance > 1 {
+				nextCharPlacement++
+				resultLength++
+			}
 
-        if i == len(chars) - 1 && lastIsDiff {
-          chars[nextCharPlacement] = chars[i]
-        }
-      }
+			if i == len(chars)-1 && lastIsDiff {
+				chars[nextCharPlacement] = chars[i]
+			}
 
 		}
 
@@ -78,8 +79,8 @@ func compress(chars []byte) int {
 }
 
 func main() {
-	arr := []byte{'a', 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'}
-  // arr := []byte{'a', 'a', 'a'}
+	arr := []byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'a', 'b', 'c'}
+	// arr := []byte{'a', 'a', 'a'}
 	length := compress(arr)
 
 	fmt.Printf("compressed %s, length %d", arr, length)
