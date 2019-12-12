6d8a8a631bd0b4c51abbf7ccd88e44cff9b891af
Jean Carlos <jhenriquez2204@gmail.com>
2019-12-12 02:54:47 +0000

    Pushing progress.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "strconv"
+import ("strconv"
+        "fmt"
+)
 
 func addNum (arr[] byte, num int) int {
 	numStr := strconv.Itoa(num)
@@ -18,17 +20,40 @@ func compress(chars []byte) int {
 		return 0
 	}
 
-	nextWriteIndex := 1
+  if len(chars) == 1 {
+    return 1
+  }
+
 	currCharIndex := 0
+  resultLength:=0
 
 	for i := 1; i < len(chars); i++ {
 
-		if chars[i] != chars[currCharIndex] {
+		if chars[i] != chars[currCharIndex] || (i == len(chars)-1 && i-currCharIndex >= 1) {
+      var subArr []byte
+      var numSteps int
 
+      if i < len(chars)-1 {
+        subArr = chars[currCharIndex+1:i]
+        numSteps = i - (currCharIndex) 
+      } else {
+        subArr = chars[currCharIndex+1:i+1]
+        numSteps = (i+1) - currCharIndex
+      }
+      numLength := addNum(subArr, numSteps)
+      currCharIndex+=(numLength+1)
+      chars[currCharIndex] = chars[i]
+      resultLength+=(numLength+1)
 		}
+
 	}
+
+  return resultLength
 }
 
 func main() {
+ arr := []byte{'a', 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c'}
+ length := compress(arr)
 
+ fmt.Printf("compressed %s, length %d", arr, length)
 }
